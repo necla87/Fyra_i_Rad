6bfa4a87d33ea044ccd2aea8c16ad3d9877339cb
SpaceDaze01 <luunaastar@gmail.com>
2024-08-28 10:45:01 +0200

    update

--- tests/app.test.js
+++ tests/app.test.js
@@ -9,7 +9,6 @@ import App from '../classes/app.js';
 
 
 
-//test players name
 test("App ska fråga efter spelarnas namn", () => {
   setMockAnswers('William', 'Erika', 'end-test');
   expect(() => new App()).toThrow('end-test');
@@ -19,7 +18,6 @@ test("App ska fråga efter spelarnas namn", () => {
 
 
 
-//test that the name is not undefined
 test("Kolla att spelarna har skrivit in något namn", () => {
   setMockAnswers('', 'end-test');
   expect(() => new App()).toThrow('end-test');
@@ -28,7 +26,6 @@ test("Kolla att spelarna har skrivit in något namn", () => {
 
 
 
-//test play again
 test("Programmet ska fråga spelarna om de vill spela igen (ja/nej)", () => {
   setMockAnswers('ja', 'nej', 'end-test');
     if ('nej') {
@@ -38,7 +35,6 @@ test("Programmet ska fråga spelarna om de vill spela igen (ja/nej)", () => {
 
 
 
-//test that the players write a string and not numbers or/and symbols
 test("Kolla att spelarna har inte skrivit nummer eller symboler", () => {
   setMockAnswers(/[^a-zA-Z\s]/, 'end-test');
   expect(() => new App()).toThrow('end-test');
