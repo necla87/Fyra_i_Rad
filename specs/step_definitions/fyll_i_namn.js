1aa46a95dbc5763e323dc052a625025c6ba8f4b9
SpaceDaze01 <luunaastar@gmail.com>
2024-09-11 11:50:30 +0200

    update

--- specs/step_definitions/fyll_i_namn.js
+++ specs/step_definitions/fyll_i_namn.js
@@ -9,17 +9,27 @@ Given('att användaren är på startsidan', () => {
 
 When('användaren ska skriva in sina namn', () => {
   // TODO: implement step
-
+  //behöver man skapa en class eller id?
+  //ändra #input
+  cy.get('input[name="answer"]').type('Tara' + '{enter}');
+  cy.wait(1000);
+  cy.get('input[name="answer"]').type('Robin' + '{enter}');
 });
 
 Then('ska användarens namn registreras och sparas', () => {
   // TODO: implement step
+  //FRÅGA
 });
 
 Then('spelet ska starta igång', () => {
   // TODO: implement step
+  cy.get('div.board')
+    .should('be.visible'); 
 });
 
 Then('spelet ska visa i text vems tur det är att börja', () => {
   // TODO: implement step
+  cy.wait(100)
+  cy.get('p')
+  .contains('Tara')
 });
