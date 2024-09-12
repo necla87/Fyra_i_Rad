06efd44314df293d044cff44b461ca6835e65ae3
SpaceDaze01 <luunaastar@gmail.com>
2024-09-12 13:30:47 +0200

    update

--- specs/step_definitions/vinnare_meddelande.js
+++ specs/step_definitions/vinnare_meddelande.js
@@ -1,13 +1,11 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given('att användaren är inne på hemsidan', () => {
-  // TODO: implement step
   cy.visit('/')
 });
 
 
 When('spelarna har skrivit in sina namn', () => {
-  // TODO: implement step
   cy.get('input[name="answer"]').type('Tara' + '{enter}');
   cy.wait(1000);
   cy.get('input[name="answer"]').type('Robin' + '{enter}');
@@ -15,14 +13,12 @@ When('spelarna har skrivit in sina namn', () => {
 
 
 When('spelet startar', () => {
-  // TODO: implement step
   cy.get('div.board')
     .should('be.visible');
 });
 
 
 When('det går att se vem som ska börja spelet', () => {
-  // TODO: implement step
   cy.wait(100)
   cy.get('p')
   cy.wait(100)
@@ -31,7 +27,6 @@ When('det går att se vem som ska börja spelet', () => {
 
 
 When('man börjar spela', () => {
-  // TODO: implement step
   cy.get(':nth-child(6) > [onclick="makeMoveOnClick(2)"]').click();
   cy.wait(500)
   cy.get(':nth-child(6) > [onclick="makeMoveOnClick(3)"]').click();
@@ -49,7 +44,6 @@ When('man börjar spela', () => {
 
 
 When('någon av spelarna får fyra i rad, horizontellt, vertikalt eller diagonalt', () => {
-  // TODO: implement step
   cy.get(':nth-child(6) > [onclick="makeMoveOnClick(2)"]')
   cy.get(':nth-child(5) > [onclick="makeMoveOnClick(2)"]')
   cy.get(':nth-child(4) > [onclick="makeMoveOnClick(2)"]')
@@ -58,7 +52,6 @@ When('någon av spelarna får fyra i rad, horizontellt, vertikalt eller diagonal
 
 
 Then('då dyker det upp en text där det står vem som har vunnit spelet', () => {
-  // TODO: implement step
   cy.wait(100)
   cy.get('p')
   cy.wait(100)
