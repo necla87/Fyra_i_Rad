4c0388030a176bccd0f3bcac782c10af5bdaee01
KubraErcin <121133657+Kubra2021@users.noreply.github.com>
2024-09-12 13:15:21 +0200

    test2.2

---
+++ specs/step_definitions/visa_vinnaren.js
@@ -0,0 +1,36 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('att spelet har startat och användaren har angett sitt namn "ANNA" och "OLLE"', (name1, name2) => {
+  cy.visit('http://localhost:5500'); 
+  cy.get('dialog').find('input[name="answer"]').type('ANNA{enter}');
+  cy.wait(1000);
+
+  cy.get('dialog').find('input[name="answer"]').type('OLLE{enter}');
+  cy.wait(1000);
+});
+
+
+Given('spelet är i gång', () => {
+  cy.get('main').should('exist'); 
+});
+
+When('Anna placerar sin bricka på en vinnande position', () => {
+  
+  cy.get('.board').find('.cell').eq(0).click(); 
+  cy.get('.board').find('.cell').eq(1).click();
+  cy.get('.board').find('.cell').eq(0).click();
+  cy.get('.board').find('.cell').eq(1).click();
+  cy.get('.board').find('.cell').eq(0).click();
+  cy.get('.board').find('.cell').eq(1).click();
+  cy.get('.board').find('.cell').eq(0).click();
+});
+
+
+When('spelet avslutas med en vinnande kombination', () => {
+  cy.get('p').should('contain.text', 'vann'); 
+});
+
+
+Then('ska spelet visa "X: ANNA vann!"', (expectedText) => {
+  cy.get('p').should('contain.text', expectedText); 
+});
---
+++ specs/visa_vinnaren.feature
@@ -0,0 +1,12 @@
+Feature: Ange namn vid spelets början
+  Som användare
+  Vill jag kunna att datorn kan använda sig av det för att meddela vems tur det är och vem som har vunnit.
+
+
+  Scenario: Meddela vinnaren med namn
+
+    Given att spelet har startat och användaren har angett sitt namn "ANNA" och "OLLE"
+    And spelet är i gång
+    When Anna placerar sin bricka på en vinnande position
+    And spelet avslutas med en vinnande kombination
+    Then ska spelet visa "X: ANNA vann!"
