549474993bb2a65569e2ea5faeff3ee4c113ed56
SpaceDaze01 <luunaastar@gmail.com>
2024-09-12 13:42:13 +0200

    update

--- specs/step_definitions/nya_spelare_knappen.js
+++ specs/step_definitions/nya_spelare_knappen.js
@@ -4,16 +4,19 @@ Given('att användare är på start sidan', () => {
   cy.visit('/')
 });
 
+
 When('spelarna har angett sina namn', () => {
   cy.get('input[name="answer"]').type('Tara' + '{enter}');
   cy.wait(1000);
   cy.get('input[name="answer"]').type('Robin' + '{enter}');
 });
 
+
 When('spelet körs igång', () => {
   cy.get('div.board')
     .should('be.visible');
 });
+
 
 When('det går att se vems tur det är att börja', () => {
   cy.wait(100)
@@ -21,6 +24,7 @@ When('det går att se vems tur det är att börja', () => {
   cy.wait(100)
     .contains('Tara')
 });
+
 
 When('spelets körs igång', () => {
   cy.get(':nth-child(6) > [onclick="makeMoveOnClick(2)"]').click();
@@ -38,12 +42,14 @@ When('spelets körs igång', () => {
   cy.get(':nth-child(3) > [onclick="makeMoveOnClick(2)"]').click();
 });
 
+
 When('någon av spelarna får fyra i rad', () => {
   cy.get(':nth-child(6) > [onclick="makeMoveOnClick(2)"]')
   cy.get(':nth-child(5) > [onclick="makeMoveOnClick(2)"]')
   cy.get(':nth-child(4) > [onclick="makeMoveOnClick(2)"]')
   cy.get(':nth-child(3) > [onclick="makeMoveOnClick(2)"]')
 });
+
 
 Then('ska användaren få möjlighet att kunna välja nya spelare genom att trycka på knappen', () => {
   cy.wait(2000)
