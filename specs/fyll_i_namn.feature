88331b5ee409d40a539e595ab8c518fd19e8f257
SpaceDaze01 <luunaastar@gmail.com>
2024-09-12 13:14:36 +0200

    update

--- specs/fyll_i_namn.feature
+++ specs/fyll_i_namn.feature
@@ -6,6 +6,5 @@ Feature: Skriv in spelarnas namn
     Scenario: Användare ska kunna skriva in sina namn i rutan
     Given att användaren är på startsidan
     When användaren ska skriva in sina namn
-    Then ska användarens namn registreras och sparas 
-    And spelet ska starta igång
+    Then spelet ska starta igång
     And spelet ska visa i text vems tur det är att börja
--- specs/step_definitions/fyll_i_namn.js
+++ specs/step_definitions/fyll_i_namn.js
@@ -1,32 +1,24 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 Given('att användaren är på startsidan', () => {
-  // TODO: implement step
   cy.visit('/') 
 });
 
+
 When('användaren ska skriva in sina namn', () => {
-  // TODO: implement step
-  //behöver man skapa en class eller id?
-  //ändra #input
   cy.get('input[name="answer"]').type('Tara' + '{enter}');
   cy.wait(1000);
   cy.get('input[name="answer"]').type('Robin' + '{enter}');
 });
 
-Then('ska användarens namn registreras och sparas', () => {
-  // TODO: implement step
-  //FRÅGA
-});
 
 Then('spelet ska starta igång', () => {
-  // TODO: implement step
   cy.get('div.board')
     .should('be.visible'); 
 });
 
+
 Then('spelet ska visa i text vems tur det är att börja', () => {
-  // TODO: implement step
   cy.wait(100)
   cy.get('p')
   cy.wait(100)
