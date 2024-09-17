d96d733e75ad9f10bc4d6e7051fd2ceb002af01b
SaraSorlin <sorlin82@gmail.com>
2024-09-17 12:30:09 +0200

    uppdate

--- specs/step_definitions/alternativa_knappar.js
+++ specs/step_definitions/alternativa_knappar.js
@@ -43,7 +43,7 @@ Then('användaren ska få upp tre alternativa knappar att välja', () => {
   cy.wait(2000);
   cy.get('.dialog-content');
   cy.get('form > .buttons');
-  cy.get('.Fortsätta');
-  cy.get('.Spela');
-  cy.get('.Nya');
+  cy.get('.Fortsätta').should('contain.text', 'Fortsätta');
+  cy.get('.Spela').should('contain.text', 'Spela');
+  cy.get('.Nya').should('contain.text', 'Nya');
 });
