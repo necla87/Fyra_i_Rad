7c6b63893ed017f9f3497fdbad0c37dc38ac1465
KubraErcin <121133657+Kubra2021@users.noreply.github.com>
2024-10-08 21:27:56 +0200

    UPDATE

--- specs/step_definitions/starta_online_spel.js
+++ specs/step_definitions/starta_online_spel.js
@@ -2,25 +2,36 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { getIframeBody } from '../helpers/iframe.js';
 
 Given('att användaren är på huvudsidan för Fyra i Rad', () => {
-  // TODO: implement step
+  cy.visit('/iframed-network-play.html');
 });
 
 When('användaren väljer att spela online', () => {
-  // TODO: implement step
-});
+
+  getIframeBody('iframe#playerX').find('.button.Ja').click();
+ 
+  });
 
 When('användaren väljer att {string}', (a) => {
-  // TODO: implement step
+
+  getIframeBody('iframe#playerX').find('.button.Skapa').click();
 });
 
 When('användaren skriver in sitt namn som {string}', (a) => {
-  // TODO: implement step
+
+  getIframeBody('iframe#playerX').find('input[name="answer"]').type('Spelare1{enter}');
+  cy.wait(500);
 });
 
 Then('ska användaren få en gå med-kod att skicka till sin vän', () => {
-  // TODO: implement step
+  
+  getIframeBody('iframe#playerX').find('input[name="joinCode"]').then((element) => {
+    let joinCode = element.val();
+
+    getIframeBody('iframe#playerX').find('.button.OK').click();
+  });
 });
 
 Then('användaren ska se att spelet väntar på att en vän ska gå med', () => {
-  // TODO: implement step
-});
+   
+  getIframeBody('iframe#playerX').contains('Väntar på att din vän ska gå med...').should('be.visible');
+  });
