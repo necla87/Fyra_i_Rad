4c73af924ca59ab149fb198eabebfdeacacc5857
necla <123477628+necla87@users.noreply.github.com>
2024-10-08 21:43:55 +0200

    test is done

--- specs/nätverksspel.feature
+++ specs/nätverksspel.feature
@@ -1,18 +1,8 @@
-Feature: Nätverksspel för Fyra i Rad-spelet
-  Som en användare vill jag kunna spela Fyra i Rad mot en annan användare över nätverket
-  så att vi kan njuta av spelet tillsammans på distans.
+Feature: Spela mot en annan användare via nätverk/internet
 
-  Scenario: Starta ett nytt nätverksspel och spela mot en vän
-     Given att användare är på huvudsidan för Fyra i Rad
-     When  användaren väljer att spela online
-     And   användaren väljer att "Skapa ett nytt spel"
-     And   användare skriver in sitt namn som "Spelare 1"
-     Then  ska användare få en gå med-kod att skicka till sin vän
-     When  Spelare 2 går med hjälp av gå med-koden
-     Then  ska båda spelarna vara anslutna
-     And   användaren ska se spelbrädet
-     When  Spelare 1 gör det första draget
-     Then  ska Spelare 2 kunna se det uppdaterade spelbrädet
-     When  Spelare 2 gör sitt drag
-     Then  ska Spelare 1 kunna se det uppdaterade spelbrädet
-     And   spelet fortsätter tills en spelare vinner eller brädet är fullt
+  Scenario: Spelare kan spela fyra i rad och få en vinnare
+    Given att vi är två spelare, där en skapar ett spel och den andra ansluter sig
+    When Spelare 1 lägger sin pjäs i en kolumn
+    And Spelare 2 lägger sin pjäs i en annan kolumn
+    And spelet fortsätter tills en av spelarna får fyra i rad
+    And spelet ska meddela att vinnaren är den spelare som fick fyra i rad
--- specs/step_definitions/nätverksspel.js
+++ specs/step_definitions/nätverksspel.js
@@ -1,54 +1,59 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { getIframeBody } from '../helpers/iframe.js';
 
-Given('att användare är på huvudsidan för Fyra i Rad', () => {
-  // TODO: implement step
+Given('att vi är två spelare, där en skapar ett spel och den andra ansluter sig', () => {
+  // Visit the page with two iframes for network play
+  cy.visit('/iframed-network-play.html');
+
+  // Player X (first player) starts a network game and gets the code
+  getIframeBody('iframe#playerX').find('.button.Ja').click();
+  getIframeBody('iframe#playerX').find('.button.Skapa').click();
+  getIframeBody('iframe#playerX').find('input[name="answer"]').type('Lisa{enter}');
+  getIframeBody('iframe#playerX').find('input[name="joinCode"]').then((element) => {
+    const joinCode = element.val(); // Retrieve the join code
+    cy.wait(1000);
+
+    // Player O (second player) joins the game using the join code
+    getIframeBody('iframe#playerO').find('.button.Ja').click();
+    getIframeBody('iframe#playerO').find('.button.Gå').click();
+    getIframeBody('iframe#playerO').find('input[name="answer"]').type('Lina{enter}');
+    getIframeBody('iframe#playerO').find('dialog:contains("gå med kod") input[name="answer"]')
+      .type(joinCode + '{enter}');
+    cy.wait(1000);
+
+  });
 });
 
-When('användaren väljer att spela online', () => {
-  // TODO: implement step
+When('Spelare 1 lägger sin pjäs i en kolumn', () => {
+  // Player X (first player) places a piece in a column (e.g., column 0)
+  getIframeBody('iframe#playerX').find('.board .cell').eq(3).click();
+  cy.wait(1000);
 });
 
-When('användaren väljer att {string}', (a) => {
-  // TODO: implement step
+When('Spelare 2 lägger sin pjäs i en annan kolumn', () => {
+  // Player O (second player) places a piece in a different column (e.g., column 1)
+ 
+  getIframeBody('iframe#playerO').find('.board .cell').eq(6).click();
+  cy.wait(1000);
+
 });
 
-When('användare skriver in sitt namn som {string}', (a) => {
-  // TODO: implement step
+When('spelet fortsätter tills en av spelarna får fyra i rad', () => {
+  getIframeBody('iframe#playerX').find('.board .cell').eq(3).click();
+  cy.wait(1000);
+  getIframeBody('iframe#playerO').find('.board .cell').eq(6).click();
+  cy.wait(1000);
+  getIframeBody('iframe#playerX').find('.board .cell').eq(3).click();
+  cy.wait(1000);
+  getIframeBody('iframe#playerO').find('.board .cell').eq(6).click();
+  cy.wait(1000);
+  getIframeBody('iframe#playerX').find('.board .cell').eq(3).click();
+  cy.wait(1000);
+  
 });
 
-Then('ska användare få en gå med-kod att skicka till sin vän', () => {
-  // TODO: implement step
-});
-
-When('Spelare 2 går med hjälp av gå med-koden', () => {
-  // TODO: implement step
-});
-
-Then('ska båda spelarna vara anslutna', () => {
-  // TODO: implement step
-});
-
-Then('användaren ska se spelbrädet', () => {
-  // TODO: implement step
-});
-
-When('Spelare 1 gör det första draget', () => {
-  // TODO: implement step
-});
-
-Then('ska Spelare 2 kunna se det uppdaterade spelbrädet', () => {
-  // TODO: implement step
-});
-
-When('Spelare 2 gör sitt drag', () => {
-  // TODO: implement step
-});
-
-Then('ska Spelare 1 kunna se det uppdaterade spelbrädet', () => {
-  // TODO: implement step
-});
-
-Then('spelet fortsätter tills en spelare vinner eller brädet är fullt', () => {
-  // TODO: implement step
+Then('spelet ska meddela att vinnaren är den spelare som fick fyra i rad', () => {
+  // Verify that a message is shown for Player X and/or Player O announcing the winner
+  getIframeBody('iframe#playerX').find('p:contains("Lisa vann!")').should('exist');
+  getIframeBody('iframe#playerO').find('p:contains("Lisa vann!")').should('exist');
 });
